87a1d457d8cd174707c049f54a4ad834d6a02a7e
tanghulu-dev <tanghulu.dev@proton.me>
2023-10-03 12:24:29 +0900

    modify

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,10 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Gabarito:wght@400;800&display=swap" rel="stylesheet"></link>
         <link href="https://fonts.googleapis.com/css2?family=Cherry+Bomb+One&family=M+PLUS+1p:wght@400;800;900&display=swap" rel="stylesheet"></link>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest"></link>
       </head>
 
       <body className={inter.className}>{children}</body>
